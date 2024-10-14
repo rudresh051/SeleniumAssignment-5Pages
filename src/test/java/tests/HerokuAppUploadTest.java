4a4e3061f55228866550acb05f26b09938b5afc4
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-10-14 23:14:33 +0530

    test case updated

---
+++ src/test/java/tests/HerokuAppUploadTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pageObjects.HerokuAppUpload;
+
+public class HerokuAppUploadTest extends BaseTest{
+	private HerokuAppUpload herokuAppUploadVariable;
+	
+	
+	@BeforeClass
+	public void setUpHerokuApp() {
+	   ((JavascriptExecutor) driver).executeScript("window.open('https://the-internet.herokuapp.com/upload', '_blank');");
+
+
+	 // Switch to the new tab
+	    for (String windowHandle : driver.getWindowHandles()) {
+	        driver.switchTo().window(windowHandle);
+	    }
+	    
+    // Initialize HerokuAppUpload page object
+	    herokuAppUploadVariable = new HerokuAppUpload(driver);
+	    
+	}
+	
+	@Test(priority = 1, groups = {"heroku"})
+	public void TestCaseVerifyFileUpload() {
+		String filePath = "C:\\selsc\\upload_file.png";
+		
+		herokuAppUploadVariable.uploadFile(filePath);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
--- src/test/java/tests/PracticeFormTestCases.java
+++ src/test/java/tests/PracticeFormTestCases.java
@@ -14,71 +14,72 @@ public class PracticeFormTestCases extends BaseTest {
 		PracticeFormVariable = new PracticeForm(driver);
 	}
 	
-	@Test(priority = 1)
+	@Test(priority = 1, groups = {"demoqa"})
 	public void TestCaseEnterFirstName() throws InterruptedException {
-		PracticeFormVariable.clickOnFirstNameInput();
+//		PracticeFormVariable.clickOnFirstNameInput();
+		Thread.sleep(3000);
 		PracticeFormVariable.clickOnFirstNameInput("Rudreshwar");
 	}
 	
-	@Test(priority = 2)
+	@Test(priority = 2, groups = {"demoqa"})
 	public void TestCaseEnterLastName() {
 		PracticeFormVariable.clickOnLastNameInput("Jha");
 	}
 	
-	@Test(priority = 3)
+	@Test(priority = 3, groups = {"demoqa"})
 	public void TestCaseEnterUserEmail() {
 		PracticeFormVariable.clickOnEmailInput("test@demoqa.com");
 	}
 	
-	@Test(priority = 4)
+	@Test(priority = 4, groups = {"demoqa"})
 	public void TestCaseSelectGender() throws InterruptedException {
 		PracticeFormVariable.clickOnGenderButton();
 	}
 	
-	@Test(priority = 5)
+	@Test(priority = 5, groups = {"demoqa"})
 	public void TestCaseEnterNumber() throws InterruptedException {
 		PracticeFormVariable.enterUserNumber();
 	}
 	
-	@Test(priority = 6)
+	@Test(priority = 6, groups = {"demoqa"})
 	public void TestCaseEnterDOB() throws InterruptedException {
 		PracticeFormVariable.enterDateOfBirth();
 		Thread.sleep(3000);
 	}
 	
-	@Test(priority = 7)
+	@Test(priority = 7, groups = {"demoqa"})
 	public void TestCaseEnterSubjects() {
 		PracticeFormVariable.enterUserSubjects();
 	}
 	
-	@Test(priority = 8)
+	@Test(priority = 8, groups = {"demoqa"})
 	public void TestCaseCheckHobbies1() {
 		PracticeFormVariable.checkUserHobbies1();
 	}
 	
-	@Test(priority = 9)
+	@Test(priority = 9, groups = {"demoqa"})
 	public void TestCaseCheckHobbies2() {
 		PracticeFormVariable.checkUserHobbies2();
 	}
 	
-	@Test(priority = 10)
+	@Test(priority = 10, groups = {"demoqa"})
 	public void TestCaseCheckHobbies3() throws InterruptedException {
 		PracticeFormVariable.checkUserHobbies3();
 		Thread.sleep(3000);
 	}
 	
-	@Test(priority = 11)
+	@Test(priority = 11, groups = {"demoqa"})
 	public void TestCaseUploadProfilePicture() throws InterruptedException {
 		PracticeFormVariable.uploadProfilePicture();
 	}
 	
-	@Test(priority = 12)
+	@Test(priority = 12, groups = {"demoqa"})
 	public void TestCaseEnterUserAddress() {
 		PracticeFormVariable.enterUserCurrentAddress("Bengaluru, Karnataka");
 	}
 	
 	
-	@Test(priority = 13)
+	@Test(priority = 13, groups = {"demoqa"})
 	public void TestCaseEnterUserSateAndCity() throws InterruptedException {
 		PracticeFormVariable.enterUserStateAndCity();
 	}
