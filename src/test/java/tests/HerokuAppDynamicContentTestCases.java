d1d7362eb8b5ceaf0a2f8983dd58c926f6127715
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-10-14 23:59:13 +0530

    test case update

--- src/test/java/tests/HerokuAppDynamicContentTestCases.java
+++ src/test/java/tests/HerokuAppDynamicContentTestCases.java
@@ -4,17 +4,30 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.BeforeClass;
 
 import pageObjects.HerokuAppDynamicContent;
+import pageObjects.HerokuAppUpload;
 
 public class HerokuAppDynamicContentTestCases {
 	private HerokuAppDynamicContent herokuAppDynamicContentVariable;
 	
 	@BeforeClass
-	public void setUpHerokuApp() {
-	   ((JavascriptExecutor) driver).executeScript("window.open('https://the-internet.herokuapp.com/dynamic_content, '_blank');");
-
-
-	 // Switch to the new tab
-	    for (String windowHandle : driver.getWindowHandles()) {
-	        driver.switchTo().window(windowHandle);
-	    }
+	public void setUpDynamicContent() {
+		   ((JavascriptExecutor) driver).executeScript("window.open('https://the-internet.herokuapp.com/dynamic_content', '_blank');");
+		// Switch to the new tab
+		    for (String windowHandle : driver.getWindowHandles()) {
+		        driver.switchTo().window(windowHandle);
+		    }
+		    
+		 // Initialize HerokuAppDynamicContent page object
+		    herokuAppDynamicContentVariable = new HerokuAppDynamicContent(null)
+		    
+		}
+	
+	
+	@Test(priority = 15)
+	public void TestCaseVerifyDynamicContent{
+		
+	
+	
+	}
+	
 }
