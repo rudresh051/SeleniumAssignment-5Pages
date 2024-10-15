78eca816a521dda0eab2c9302cd56149ebc95e55
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-10-15 23:51:40 +0530

    test case added

--- src/test/java/tests/HerokuAppDynamicControlsTestCases.java
+++ src/test/java/tests/HerokuAppDynamicControlsTestCases.java
@@ -1,5 +1,34 @@
 package tests;
 
-public class HerokuAppDynamicControlsTestCases {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import pageObjects.HerokuAppDynamicControls;
+
+public class HerokuAppDynamicControlsTestCases {
+	private HerokuAppDynamicControls herokuAppDynamicControlsVariable;
+	
+	@BeforeClass
+	public void setUpDynamicControl() {
+		   ((JavascriptExecutor) driver).executeScript("window.open('https://the-internet.herokuapp.com/dynamic_controls', '_blank');");
+
+		// Switch to the new tab
+		    for (String windowHandle : driver.getWindowHandles()) {
+		        driver.switchTo().window(windowHandle);
+		    }
+		    
+		    
+		    herokuAppDynamicControlsVariable = new HerokuAppDynamicControls(driver);
+		    
+	}
+	
+	@Test(priority = 16)
+	public void TestCaseVerifyCheckbox() {
+		herokuAppDynamicControlsVariable.clickOnCheckBox();
+	}
+	
+	
+	
 }
